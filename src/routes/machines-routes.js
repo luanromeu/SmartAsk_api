b416689403fe884ea70b4c4dea0eb8dd5e2023ee
luanromeu <luanromeu59@gmail.com>
2018-12-19 13:10:28 -0200

    merge

--- src/routes/machines-routes.js
+++ src/routes/machines-routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/machines-controller');
 const authService = require('../../src/services/auth-service');
 
-router.get('/list',controller.list)
+router.get('/list', authService.authorize,controller.list)
 
 
 module.exports = router;
